7111afe51051b2006ddd6012276684a118b79f00
Elena Kulgavaya <93939364+elenakulgavaya@users.noreply.github.com>
2026-05-09 17:30:42 +0200

    Migrate to surety-api (#9)
    * Migrate to surety-api
    
    * fix pylint issues

--- src/fildapi/__init__.py
+++ src/fildapi/__init__.py
@@ -1,3 +1,17 @@
+import warnings
+
+# pylint: disable=wrong-import-position
+warnings.warn(
+    "fild-api-hub is deprecated and will no longer be maintained. "
+    "Migrate to surety-api: pip install surety-api. "
+    "Replace 'from fildapi import ApiCaller, ApiMethod, MockServer, HttpMethod' "
+    "with 'from surety.api import ApiCaller, ApiContract, MockServer, HttpMethod'. "
+    "Note: ApiMethod was renamed to ApiContract in surety-api. "
+    "See https://github.com/elenakulgavaya/surety-api for details.",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
 from .caller import ApiCaller
 from .mock.service import MockServer
 from .method import ApiMethod
